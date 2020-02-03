33c249842fa91710fa652dc369f653d43aa00e25
pavithraRanasinghe <paviranasinghe20@gmail.com>
2020-02-04 01:07:15 +0530

    update customer

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 const mongoose = require('./../db/mongoose');
 const Customer = require('./../models/customer');
@@ -31,7 +32,22 @@ app.get('/customer', (req, res) => {
     });
 });
 
-app.
+app.delete('/customer/:id',(req,res)=>{
+   const id = req.params.id;
+
+   if (!ObjectID.isValid(id)){
+       return res.status(404).send("error");
+   }
+
+   Customer.findByIdAndRemove(id).then((customer)=>{
+       if (!customer){
+           return res.status(404).send();
+       }
+       res.send(customer);
+   },(err)=>{
+        res.status(404).send(err);
+   });
+});
 
 app.listen(8080, () => {
     console.log("Server up on port 8080");
